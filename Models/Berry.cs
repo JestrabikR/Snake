7c9a53739d2801e0990ef523fc6af0a0c4e3a4ae
JestrabikR <jestrab2003@gmail.com>
2026-04-20 20:20:22 +0200

    Added Berry class

---
+++ Models/Berry.cs
@@ -0,0 +1,17 @@
+﻿namespace Snake.Models;
+
+public class Berry(int boardWidth, int boardHeight)
+{
+    public Position Position { get; private set; } = GeneratePosition(boardWidth, boardHeight);
+
+    public void Relocate(int boardWidth, int boardHeight)
+        => Position = GeneratePosition(boardWidth, boardHeight);
+
+    private static Position GeneratePosition(int boardWidth, int boardHeight)
+    {
+        return new Position(
+            Random.Shared.Next(1, boardWidth - 1),
+            Random.Shared.Next(1, boardHeight - 1)
+        );
+    }
+}
