4a3b12ab4057d01bd86c2dd6569300c92f7cb839
JestrabikR <jestrab2003@gmail.com>
2026-04-20 20:34:05 +0200

    Added GameState

---
+++ Game/GameState.cs
@@ -0,0 +1,55 @@
+﻿namespace Snake.Game;
+
+using Enums;
+using Models;
+
+public class GameState
+{
+    private readonly Snake _snake;
+    private readonly Berry _berry;
+
+    public int BoardWidth { get; }
+    public int BoardHeight { get; }
+    public int Score { get; private set; }
+    public bool IsGameOver { get; private set; }
+
+    public Position SnakeHead => _snake.Head;
+    public IReadOnlyList<Position> SnakeBody => _snake.Segments;
+    public Position BerryPosition => _berry.Position;
+
+    public GameState(int boardWidth, int boardHeight)
+    {
+        BoardWidth = boardWidth;
+        BoardHeight = boardHeight;
+
+        var center = new Position(boardWidth / 2, boardHeight / 2);
+        _snake = new Snake(center, GameConfig.InitialSnakeLength);
+        _berry = new Berry(boardWidth, boardHeight);
+    }
+
+    public void Tick(Direction direction)
+    {
+        _snake.Move(direction);
+
+        if (HitsWall() || _snake.CollidesWithSelf())
+        {
+            IsGameOver = true;
+            return;
+        }
+
+        if (SnakeHead != BerryPosition)
+        {
+            return;
+        }
+
+        Score++;
+        _snake.Grow();
+        _berry.Relocate(BoardWidth, BoardHeight);
+    }
+
+    private bool HitsWall()
+    {
+        return SnakeHead.X == 0 || SnakeHead.X == BoardWidth - 1
+                || SnakeHead.Y == 0 || SnakeHead.Y == BoardHeight - 1;
+    }
+}
