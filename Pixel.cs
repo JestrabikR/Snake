abc1beae6df55c26b169cfa875ed929d2f35bb10
JestrabikR <jestrab2003@gmail.com>
2026-04-20 20:59:58 +0200

    Removed unused Pixel class

--- Pixel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Snake;
-
-public class Pixel(Position position, ConsoleColor color)
-{
-    public int X { get; set; } = position.X;
-    public int Y { get; set; } = position.Y;
-    public ConsoleColor Color { get; set; } = color;
-}
